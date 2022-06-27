210c120019611dcf8d04db3518f233cd207362ab
Arnaud Gissinger <37625778+mathix420@users.noreply.github.com>
2022-06-27 20:28:44 +0200

    [FIX] sizing padding and margins

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,9 @@ export default {
 
 <style>
 html, body, #__nuxt, #__layout, main, iframe {
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
